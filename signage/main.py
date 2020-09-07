5b378e20c2184ef6e9710fa60f26d6a65b97cd83
Yuji Azama <yuji.azama@gmail.com>
2020-09-07 11:33:56 +0900

    update signage/main.py

--- signage/main.py
+++ signage/main.py
@@ -13,10 +13,11 @@ app = Flask(__name__)
 
 
 @app.route('/image', methods=['POST'])
-def update():
+def update_image():
     body = request.json
-    update_image(body['url'])
-    return ""
+    img = image_read_from_url(body['url'])
+    cv2.imshow("Loaded image", img)
+    return {'status': 'success'}
 
 
 def image_read_from_url(url):
@@ -29,19 +30,13 @@ def image_read_from_url(url):
     return img
 
 
-def update_image(url):
-    img = image_read_from_url(url)
-    cv2.imshow("Loaded image", img)
-    return
-
-
 if __name__ == '__main__':
     init_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': '5000'})
     init_thread.start()
 
-    url = 'http://192.168.28.10:30007/images/image1.jpeg'
+    url = 'https://prtimes.jp/data/corp/18705/tmp-e7e85f4f14675d37ecb0f9a80369cb4c-847d679de842d94ad6f9b6b165580419.jpg'
     img = image_read_from_url(url)
-    cv2.namedWindow("Loaded image", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Loaded image", 500, 500)
+    cv2.namedWindow("Loaded image", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('Loaded image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow("Loaded image", img)
     if cv2.waitKey() & 0xff == 27: quit()
